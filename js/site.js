46532c1ba9d6ade7f9f7859a51ecc0c05f7a0dee
Kyle Rigdon <krigdon64@gmail.com>
2024-04-10 12:03:10 -0500

    Added javascript to check for even numbers and make them bold. And display an error message if the user enters non-numbers, negative numbers and numbers higher than 100.

--- js/site.js
+++ js/site.js
@@ -2,8 +2,7 @@
 // AKA the Entry Point
 function getValues() {
   // get the input element for startNumber and grab its value
-  let startInputElement = document.getElementById('startNumber');
-  let startNumber = startInputElement.value;
+  let startNumber = document.getElementById('startNumber').value;
 
   // do the same thing for endNumber
   let endNumber = document.getElementById('endNumber').value;
@@ -11,16 +10,25 @@ function getValues() {
   startNumber = Number(startNumber);
   endNumber = Number(endNumber);
 
-  let generatedNumbers = generateValues(startNumber, endNumber);
-
-  displayValues(generatedNumbers);
+  if (isNaN(startNumber) || isNaN(endNumber) || startNumber < 1 || endNumber > 100) {
+    // display an error message
+    Swal.fire({
+      icon: "error",
+      title: "Oops!",
+      text: 'Please enter a valid number for TallyHo to use.',
+      backdrop: false
+    });
+  } else {
+    let generatedNumbers = generateValues(startNumber, endNumber);
+    displayValues(generatedNumbers);
+  }
 }
 
 // generate a list of numbers between those two values
 function generateValues(start, end) {
   let numbers = [];
 
-  for (let i = start; i <= end; i = i + 1) {
+  for (let i = start; i <= end; i++) {
        numbers.push(i);
   }
 
@@ -32,13 +40,32 @@ function displayValues(numberArray) {
 
   let tableHtml = '';
 
-  for (let index = 0; index < numberArray.length; index = index + 1) {
+  for (let index = 0; index < numberArray.length; index += 1) {
     
     let number = numberArray[index];
 
-    tableHtml = tableHtml + '<tr><td>' + number + '</td></tr>';
+    let className = '';
+    if (number % 2 == 0) {
+      // use the class 'even' in the html
+      className = 'even';
+    } else {
+      // use the class 'odd' in the html
+      className = 'odd';
+    }
+
+    tableHtml += `<tr><td class="${className}">${number}</td></tr>`;
   }
 
   let tbody = document.getElementById('results');
   tbody.innerHTML = tableHtml;
 }
+
+/*
+
+  <tr><td class="odd">1</td></tr>
+  <tr><td class="even">2</td></tr>
+  <tr><td class="odd">3</td></tr>
+  <tr><td class="even">4</td></tr>
+  <tr><td class="odd">5</td></tr>
+
+*/
